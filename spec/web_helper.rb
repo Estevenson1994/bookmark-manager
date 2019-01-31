ef378588462dd56c770d190e54fa2ce17d71e3f6
Erin Stevenson <estevenson_94@live.com>
2019-01-31 12:09:24 +0000

    11. Refactored web helper to use the add method'

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,6 +1,8 @@
+require_relative '../lib/bookmark.rb'
+
 def db_setup
   @con = PG.connect :dbname => 'bookmark_manager_test'
   @con.exec "TRUNCATE bookmarks;"
-  @con.exec "INSERT INTO bookmarks (url, title) VALUES ('http://www.makersacademy.com', 'Makers Academy'); "
-  @con.exec "INSERT INTO bookmarks (url, title) VALUES ('http://www.google.com', 'google');"
+  Bookmark.add(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+  Bookmark.add(title: 'google', url: 'http://www.google.com')
 end
